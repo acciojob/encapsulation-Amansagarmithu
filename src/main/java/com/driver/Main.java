9ee8e259c621401ae928f648e68725c56ed009a9
Aman Sagar <amansagar887@gmail.com>
2022-12-12 18:58:57 +0530

    second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,14 +4,14 @@ public class Main {
   public static void main(String[] args){
       Scanner input = new Scanner(System.in);
       RWOnly obj = new RWOnly();
-      try{
-          obj.name = input.next();
-          System.out.println(obj.name);
-
-      }
-      catch(Exception e){
-          System.out.println(e);
-      }
+//      try{
+//          obj.name = input.next();
+//          System.out.println(obj.name);
+//
+//      }
+//      catch(Exception e){
+//          System.out.println(e);
+//      }
       obj.setName(input.next());
       obj.getName();
   }
